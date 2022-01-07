cc337118c25c50443fb98ac655e1a98b8f82b958
Alexandre Oyama <94652462+alexandre-999@users.noreply.github.com>
2022-01-07 21:36:36 +0900

    課題完了

---
+++ js/FizzBuzz.js
@@ -0,0 +1,35 @@
+'user strict';
+
+
+const output = document.getElementById('output');
+
+output.addEventListener('click', () => {
+  const fizznum = Number(document.getElementById('id_FizzNum').value);
+  const buzznum = Number(document.getElementById('id_BuzzNum').value);
+  const fizzbuzzResult = document.getElementById('fizzbuzz-result');
+  fizzbuzzResult.innerHTML = '';
+
+  if(
+    Number.isInteger(fizznum) &&
+    Number.isInteger(buzznum) &&
+    fizznum > 0 &&
+    buzznum > 0
+    ) {
+      let val = ''
+      for(i = 1; i < 100; i++) {
+        if(i % fizznum === 0 && i % buzznum === 0) {
+          val = `FizzBuzz ${i}`;
+        } else if(i % fizznum === 0) {
+          val = `Fizz ${i}`;
+        } else if(i % buzznum === 0) {
+          val = `Buzz ${i}`;
+        } else {
+          val = '';
+        }
+        const li = document.createElement('li');
+        fizzbuzzResult.appendChild(li);
+        li.textContent = val;
+      }} else {
+        fizzbuzzResult.innerHTML = '';
+        fizzbuzzResult.innerHTML = '<p>整数値を入力してください<p>'
+}});
